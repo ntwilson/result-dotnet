f4ab22bd8eea1f439fd5bb7ad049315e4d5e8ba1
nathan wilson <nathan.wilson@marquette.edu>
2017-02-06 09:52:58 -0600

    tests for the Result type itself, but not for the multi-arity map and bind functions

--- ResultDotNet.Specs/ResultSpec.fs
+++ ResultDotNet.Specs/ResultSpec.fs
@@ -6,8 +6,56 @@ open NUnit.Framework
 module ResultSpec =
     open Should
     open ResultDotNet
+    open System
 
     [<Test>]
-    let works () =
-        Success 5
-        |> shouldBe (Success 5)
+    let ``should have structural equality`` () =
+        Success 5 |> shouldBe (Success 5)
+        Failure "didn't work" |> shouldBe (Failure "didn't work")
+
+        Success 5 |> shouldNotBe (Success 10)
+        Failure "didn't work" |> shouldNotBe (Failure "hello world")
+
+    
+
+    [<Test>]
+    let ``match member should return an appropriate value`` () =
+        (Success 5).Match 
+            (Func<int, string> (fun i -> i.ToString()))
+            (Func<string, string> (id))
+        |> shouldBe "5"
+        
+        (Failure "didn't work").Match 
+            (Func<int, string> (fun i -> i.ToString()))
+            (Func<string, string> (id))
+        |> shouldBe "didn't work"
+
+    [<Test>]
+    let ``map member should work with non-results`` () =
+        (Success 5).Map (Func<int, int> ((+) 3))
+        |> shouldBe (Success 8)
+
+        (Failure "didn't work").Map (Func<int, int> ((+) 3))
+        |> shouldBe (Result<int, string>.Failure "didn't work")
+
+    [<Test>]
+    let ``bind member should work with result funcs`` () =
+        (Success 5).Bind (Func<int, Result<int,string>> (fun i -> Success (i + 3)))
+        |> shouldBe (Result<int,string>.Success 8)
+
+        (Failure "didn't work").Bind (Func<int, Result<int, string>> (fun i -> Success (i + 3)))
+        |> shouldBe (Result<int, string>.Failure "didn't work")
+
+    [<Test>]
+    let ``static members behave the same as the members`` () =
+        Success 5 |> Result.Map (Func<int, int> ((+) 3))
+        |> shouldBe (Success 8)
+
+        Failure "didn't work" |> Result.Map (Func<int, int> ((+) 3))
+        |> shouldBe (Result<int, string>.Failure "didn't work")
+    
+        Success 5 |> Result.Bind (Func<int, Result<int,string>> (fun i -> Success (i + 3)))
+        |> shouldBe (Result<int,string>.Success 8)
+
+        Failure "didn't work" |> Result.Bind (Func<int, Result<int, string>> (fun i -> Success (i + 3)))
+        |> shouldBe (Result<int, string>.Failure "didn't work")
--- ResultDotNet.Specs/Should.fs
+++ ResultDotNet.Specs/Should.fs
@@ -5,3 +5,12 @@ module Should =
 
   let shouldBe expected actual = 
     Assert.AreEqual (expected, actual)
+
+  let shouldNotBe expected actual = 
+    Assert.AreNotEqual (expected, actual)
+
+  let shouldSatisfy predicate actual =
+    predicate actual |> shouldBe true
+
+  let shouldNotSatisfo predicate actual = 
+    predicate actual |> shouldNotBe true
