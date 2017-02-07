090fd280a42e913f812d2f24fa4c09664eff1175
nathan wilson <nathan.wilson@marquette.edu>
2017-02-06 23:27:19 -0600

    finish testing the existing result class

--- ResultDotNet.Specs/ResultSpec.fs
+++ ResultDotNet.Specs/ResultSpec.fs
@@ -16,8 +16,6 @@ module ResultSpec =
         Success 5 |> shouldNotBe (Success 10)
         Failure "didn't work" |> shouldNotBe (Failure "hello world")
 
-    
-
     [<Test>]
     let ``match member should return an appropriate value`` () =
         (Success 5).Match 
@@ -59,3 +57,147 @@ module ResultSpec =
 
         Failure "didn't work" |> Result.Bind (Func<int, Result<int, string>> (fun i -> Success (i + 3)))
         |> shouldBe (Result<int, string>.Failure "didn't work")
+
+    [<Test>]
+    let ``can create Results with static functions instead of the normal constructors`` ()  =
+        Result.Success 5 |> shouldBe (Success 5)
+        Result.Failure "didn't work" |> shouldBe (Failure "didn't work")
+
+    [<Test>]
+    let ``can map two results`` () =
+        Result.Map2 (Func<int, float, float> (fun i j -> (float i) + j))
+            (Success 3)
+            (Success 5.)
+        |> shouldBe (Success 8.)
+
+        Result.Map2 (Func<int, float, float> (fun i j -> (float i) + j))
+            (Failure "didn't work")
+            (Success 5.)
+        |> shouldBe (Result<float, string>.Failure "didn't work")
+        
+        Result.Map2 (Func<int, float, float> (fun i j -> (float i) + j))
+            (Success 3)
+            (Failure "didn't work")
+        |> shouldBe (Result<float, string>.Failure "didn't work")
+
+    [<Test>]
+    let ``can bind two results`` () =
+        Result.Bind2 (Func<int, float, Result<float, string>> (fun i j -> Success ((float i) + j)))
+            (Success 3)
+            (Success 5.)
+        |> shouldBe (Result<float, string>.Success 8.)
+
+        Result.Bind2 (Func<int, float, Result<float, string>> (fun i j -> Success ((float i) + j)))
+            (Failure "didn't work")
+            (Success 5.)
+        |> shouldBe (Result<float, string>.Failure "didn't work")
+        
+        Result.Bind2 (Func<int, float, Result<float, string>> (fun i j -> Success ((float i) + j)))
+            (Success 3)
+            (Failure "didn't work")
+        |> shouldBe (Result<float, string>.Failure "didn't work")
+
+    [<Test>]
+    let ``can map three results`` () =
+        Result.Map3 (Func<int, float, string, string> (fun i j k -> i.ToString() + j.ToString() + k))
+            (Success 13)
+            (Success 37.)
+            (Success "code")
+        |> shouldBe (Success "1337code")
+
+        Result.Map3 (Func<int, float, string, string> (fun i j k -> i.ToString() + j.ToString() + k))
+            (Failure "didn't work")
+            (Success 37.)
+            (Success "code")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+        
+        Result.Map3 (Func<int, float, string, string> (fun i j k -> i.ToString() + j.ToString() + k))
+            (Success 13)
+            (Success 37.)
+            (Failure "didn't work")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+    
+    [<Test>]
+    let ``can bind three results`` () =
+        Result.Bind3 
+            (Func<int, float, string, Result<string,string>> (fun i j k -> 
+                Success (i.ToString() + j.ToString() + k)))
+            (Success 13)
+            (Success 37.)
+            (Success "code")
+        |> shouldBe (Result<string, string>.Success "1337code")
+
+        Result.Bind3 
+            (Func<int, float, string, Result<string,string>> (fun i j k -> 
+                Success (i.ToString() + j.ToString() + k)))
+            (Failure "didn't work")
+            (Success 37.)
+            (Success "code")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+        
+        Result.Bind3 
+            (Func<int, float, string, Result<string,string>> (fun i j k -> 
+                Success (i.ToString() + j.ToString() + k)))
+            (Success 13)
+            (Success 37.)
+            (Failure "didn't work")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+    
+    [<Test>]
+    let ``can map four results`` () =
+        Result.Map4 
+            (Func<int, float, string, string, string> (fun i j k l -> 
+                i.ToString() + j.ToString() + k + l))
+            (Success 13)
+            (Success 37.)
+            (Success "co")
+            (Success "de")
+        |> shouldBe (Success "1337code")
+
+        Result.Map4 
+            (Func<int, float, string, string, string> (fun i j k l -> 
+                i.ToString() + j.ToString() + k + l))
+            (Failure "didn't work")
+            (Success 37.)
+            (Success "co")
+            (Success "de")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+        
+        Result.Map4 
+            (Func<int, float, string, string, string> (fun i j k l -> 
+                i.ToString() + j.ToString() + k + l))
+            (Success 13)
+            (Success 37.)
+            (Success "co")
+            (Failure "didn't work")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+    
+    [<Test>]
+    let ``can bind four results`` () =
+        Result.Bind4 
+            (Func<int, float, string, string, Result<string,string>> (fun i j k l -> 
+                Success (i.ToString() + j.ToString() + k + l)))
+            (Success 13)
+            (Success 37.)
+            (Success "co")
+            (Success "de")
+        |> shouldBe (Result<string, string>.Success "1337code")
+
+        Result.Bind4 
+            (Func<int, float, string, string, Result<string,string>> (fun i j k l -> 
+                Success (i.ToString() + j.ToString() + k + l)))
+            (Failure "didn't work")
+            (Success 37.)
+            (Success "co")
+            (Success "de")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+        
+        Result.Bind4 
+            (Func<int, float, string, string, Result<string,string>> (fun i j k l -> 
+                Success (i.ToString() + j.ToString() + k + l)))
+            (Success 13)
+            (Success 37.)
+            (Success "co")
+            (Failure "didn't work")
+        |> shouldBe (Result<string, string>.Failure "didn't work")
+    
